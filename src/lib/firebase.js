48dd8b2a5d13e6147585f235afdfa1468208b3d6
vitoria-junker <vickjunker14@gmail.com>
2023-08-24 10:29:33 -0300

    make

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getStorage, ref } from 'firebase/storage';
+import { getStorage } from 'firebase/storage';
 import dotenv from 'dotenv';
 
 dotenv.config();
